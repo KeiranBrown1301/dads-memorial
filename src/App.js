b6c39433d64e64a8ca2bac05b81a8f1e9c7f864f
Keiran Brown <keiranbrown1301@gmail.com>
2022-09-08 19:15:33 +0900

    moved browser router from index to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>John Brown</h1>
-        <p>'Alive and Well'</p>
-        <h3>25.01.1951 - 08.08.2022</h3>
-      </header>
+      <BrowserRouter>
+        <header className="App-header">
+          <h1>John Brown</h1>
+          <p>'Alive and Well'</p>
+          <h3>25.01.1951 - 08.08.2022</h3>
+        </header>
+      </BrowserRouter>
     </div>
   );
 }
